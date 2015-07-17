800dd8b5767872c4ac8e7452e25ba6acf7a78e6b
LoiNT <loint20@gmail.com>
2015-07-18 01:50:21 +0700

    FacultyBO and TeacherBO

---
+++ BusinessLogic/FacultyBO.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+
+namespace BusinessLogic
+{
+	public class FacultyBO
+	{
+		private DbDataContext db = new DbDataContext();
+		
+		// select all Faculty from DB
+		public List<Faculty> Select_All()
+		{
+			try
+			{
+				return db.Faculties.OrderByDescending(b => b.Fac_Code).ToList<Faculty>();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("FacultyBO.Select_All" + ex.ToString());
+			}
+		}
+		// select Faculty by ID
+		public List<Faculty> Select_ById(int id)
+		{
+			try
+			{
+				return db.Faculties.Where(b => b.Fac_Code == id).ToList<Faculty>();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("FacultyBO.Select_ById" + ex.ToString());
+			}
+		}
+		// select Faculty by Name
+		public List<Faculty> Select_ByName(string name)
+		{
+			try
+			{
+				return db.Faculties.Where(b => b.Fac_Name == name).ToList<Faculty>();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("FacultyBO.Select_ByID" + ex.ToString());
+			}
+		}
+
+		// insert new Faculty 
+		public void Insert(Faculty aFaculty)
+		{
+			try
+			{
+				db.Faculties.InsertOnSubmit(aFaculty);
+				db.SubmitChanges();
+			
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("FacultyBO.Insert: " + ex.ToString());
+			}
+		}
+
+		// update Faculty
+
+		public void Update(Faculty aFaculty)
+		{
+			try
+			{
+				var obj = from m in db.Faculties
+							where m.Fac_Code == aFaculty.Fac_Code
+							select m;
+				foreach (var oFa in obj)
+				{
+					oFa.Fac_Code = aFaculty.Fac_Code;
+					oFa.Fac_Name = aFaculty.Fac_Name;
+				}
+				db.SubmitChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("FacultyBO.Update: " + ex.ToString());
+			}
+		}
+
+		// delete Faculty
+		public void Delete(Faculty aFaculty)
+		{
+			try
+			{
+				var obj = from m in db.Faculties
+						  where m.Fac_Code == aFaculty.Fac_Code
+						  select m;
+				foreach (var oFa in obj)
+				{
+					db.Faculties.DeleteOnSubmit(oFa);
+				}
+				db.SubmitChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("FacultyBO.Update: " + ex.ToString());
+			}
+		}
+	}
+}
---
+++ BusinessLogic/TeacherBO.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BusinessLogic
+{
+	public class TeacherBO
+	{
+		private DbDataContext db = new DbDataContext();
+
+
+		// Select All
+		public List<Teacher> Select_All()
+		{
+			try
+			{
+				return db.Teachers.OrderByDescending(b => b.Tea_Code).ToList<Teacher>();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("TeacherBO.Select_All" + ex.ToString());
+			}
+		}
+
+		// Select by ID
+		public List<Teacher> Select_ById(int id)
+		{
+			try
+			{
+				return db.Teachers.Where(b => b.Tea_Code == id).ToList<Teacher>();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("TeacherBO.Select_ByID" + ex.ToString());
+			}
+		}
+
+		// Select by Name
+		public List<Teacher> Select_ByName(string name)
+		{
+			try
+			{
+				return  db.Teachers.Where(b => b.Tea_Name == name).ToList<Teacher>();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("TeacherBO.Select_ByName" + ex.ToString());
+			}
+		}
+		// Insert
+		public void Insert(Teacher aTeacher)
+		{
+			try
+			{
+				db.Teachers.InsertOnSubmit(aTeacher);
+				db.SubmitChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("TeacherBO.Insert" + ex.ToString());
+			}
+		} 
+		// Update
+		public void Update(Teacher aTeacher)
+		{
+			try
+			{
+				var obj = from m in db.Teachers
+						  where m.Tea_Code == aTeacher.Tea_Code
+						  select m;
+				foreach (var cur in obj)
+				{
+					cur.Tea_Code = aTeacher.Tea_Code;
+					cur.Tea_Name = aTeacher.Tea_Name;
+					cur.Tea_Address = aTeacher.Tea_Address;
+					cur.Tea_DateOfBirth = aTeacher.Tea_DateOfBirth;
+					cur.Tea_Ethnic = aTeacher.Tea_Ethnic;
+					cur.Tea_HomeTown = aTeacher.Tea_HomeTown;
+					cur.Tea_PhoneNumber = aTeacher.Tea_PhoneNumber;
+					cur.Tea_Religion = aTeacher.Tea_Religion;
+				}
+				db.SubmitChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("TeacherBO.Update" + ex.ToString());
+			}
+		}
+		// Delete
+		public void Delete(Teacher aTeacher)
+		{
+			try
+			{
+				var obj = from m in db.Teachers
+						  where m.Tea_Code == aTeacher.Tea_Code
+						  select m;
+				foreach (var cur in obj)
+				{
+					db.Teachers.DeleteOnSubmit(cur);
+				}
+				db.SubmitChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("TeacherBO.Delete" + ex.ToString());
+			}
+		}
+
+
+
+	}
+}
