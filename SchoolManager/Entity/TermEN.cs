824d1a4c881019b76f4d62af177e2d07b2c29773
LoiNT <loint20@gmail.com>
2015-07-18 23:38:45 +0700

    commit 18/7

---
+++ SchoolManager/Entity/TermEN.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace SchoolManager.Entity
+{
+	public class TermEN
+	{
+		public TermEN()
+		{
+
+		}
+		public int TermCode { get; set; }
+		public string TermName { get; set; }
+		public string TermGroup { get; set; }
+		public DateTime TermStart { get; set; }
+		public DateTime TermEnd { get; set; }
+		public void SetValue(Term aTerm)
+		{
+			this.TermCode = aTerm.Ter_Code;
+			this.TermName = aTerm.Ter_Name;
+			this.TermGroup = aTerm.Ter_Group;
+			this.TermStart = aTerm.Ter_YearStart;
+			this.TermEnd = aTerm.Ter_YearEnd;
+		}
+	}
+}
---
+++ SchoolManager/Entity/UserEN.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace SchoolManager.Entity
+{
+	public class UserEN
+	{
+		public UserEN()
+		{
+
+		}
+		public string UserName { get; set; }
+		public string UserPassWord { get; set; }
+		public void SetValue(User aUser)
+		{
+			this.UserName = aUser.Use_Name;
+			this.UserPassWord = aUser.Use_Password;
+		}
+	}
+}
