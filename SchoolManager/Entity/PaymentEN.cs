964c9e9a9d58cbc8bb51aec6470964cb2cfb7a6b
Tuan95 <tuanda29@gmail.com>
2015-07-18 23:52:40 +0700

    commit Payment va Specialization

---
+++ SchoolManager/Entity/PaymentEN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace SchoolManager.Entity {
+    class PaymentEN : Payment{
+
+        public PaymentEN( ) {
+        
+        }
+
+        public string PaymentStu_Code { get; set; }
+
+        public int PaymentTer_Code { get; set; }
+
+        public string PaymentMoney { get; set; }
+
+        public int? PaymentStatus { get; set; }
+
+        public void setValue( Payment aPayment ) {
+
+            this.PaymentStu_Code = aPayment.Stu_Code;
+            this.PaymentTer_Code = aPayment.Ter_Code;
+            this.PaymentMoney = aPayment.Pay_Money;
+            this.PaymentStatus = aPayment.Pay_Status;
+        }
+
+    }
+}
