c1853a90ad4cfac52ae2197ff83bb20b9b1aeffd
LoiNT <loint20@gmail.com>
2015-07-19 09:27:30 +0700

    Cập nhập chức năng mã hóa ở hàm enter event. Lúc trước mới chỉ thêm vào button click chứ trong enter event chưa có

--- SchoolManager/Form Main/Login.cs
+++ SchoolManager/Form Main/Login.cs
@@ -13,27 +13,14 @@ using BusinessLogic;
 
 namespace SchoolManager.Design {
     public partial class Login : DevExpress.XtraEditors.XtraForm {
-		//private DbDataContext db = new DbDataContext();
+		private DbDataContext db = new DbDataContext();
         public Login () {
             InitializeComponent();
         }
 		
 		private void btnLogin_Click(object sender, EventArgs e)
 		{
-			//var q = from m in db.Users
-			//		where m.Use_Name == txtUserName.Text && m.Use_Password == txtPassword.Text
-			//		select m;
-
-			//if (q.Any())
-			//{
-			//	Main aMain = new Main();
-			//	aMain.Show();
-			//	this.Visible = false;
-			//}
-			//else
-			//{
-			//	MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu.", "Thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			//}
+			
 			UserBO user = new UserBO();
 			string username = txtUserName.Text;
 			string password = txtPassword.Text;
@@ -51,18 +38,6 @@ namespace SchoolManager.Design {
 
         private void MatchingEnterPress (object sender, KeyEventArgs e) {
             if (e.KeyCode == Keys.Enter) {
-				//var q = from m in db.Users
-				//		where m.Use_Name == txtUserName.Text && m.Use_Password == txtPassword.Text
-				//		select m;
-
-				//if (q.Any()) {
-				//	Main aMain = new Main();
-				//	aMain.Show();
-				//	this.Visible = false;
-				//}
-				//else {
-				//	MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu.", "Thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				//}
 				UserBO user = new UserBO();
 				string username = txtUserName.Text;
 				string password = txtPassword.Text;
@@ -76,6 +51,7 @@ namespace SchoolManager.Design {
 				{
 					MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu.", "Thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
+				
             }
         }
       
